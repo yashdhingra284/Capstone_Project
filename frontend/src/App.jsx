300a9efd4388823b560cf66f2ad3b03e5518ed5a
yashdhingra284 <yashdhingrawork@gmail.com>
2025-11-07 23:17:18 +0530

    Delete frontend/src directory

--- frontend/src/App.jsx
+++
@@ -1,7 +0,0 @@
-// after redirect from provider, backend already set cookie; client should call /api/me to fetch user
-useEffect(() => {
-  fetch('/api/me', { credentials: 'include' })
-    .then(r => r.json())
-    .then(setUser)
-    .catch(()=>setUser(null));
-}, []);
